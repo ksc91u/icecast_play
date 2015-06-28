9a3edc37c109c2bf45bd5fd4e2c6221207a143a7
adolf <adolf@175-182-192-175.adsl.dynamic.seed.net.tw>
2015-06-28 11:39:50 +0800

    * play random station with genre contains 'class'

---
+++ icecast.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#http://dir.xiph.org/yp.xml
+import xml.etree.ElementTree as ET
+import random
+import os, os.path, sys
+
+
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
+
+tree = ET.parse('yp.xml')
+element_root = tree.getroot()
+
+urls = []
+
+for child in element_root:
+    for entryc in child:
+        if (entryc.tag == 'listen_url'):
+            url = entryc.text
+        if (entryc.tag == 'genre' and ('class' in entryc.text.lower())):
+            urls.append(url)
+
+playurl = random.choice(urls)
+
+ffplay = which('ffplay')
+mplayer = which('mplayer')
+if (ffplay is not None):
+    os.execl(ffplay,'ffplay','-nodisp','-bufsize','256k',url)
+elif (mplayer is not None):
+    os.execl(mplayer,'mplayer','-prefer-ipv4','-cache','256','-vo','null',url)
